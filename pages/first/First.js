54d69648a3d568febe94b6fed552363807246e26
Ivan Marakhovsky <marakhovsky.ivan@gmail.com>
2022-01-20 11:28:03 +0300

    first

---
+++ pages/first/First.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../../styles/First.module.css'
+
+export default function First() {
+  return (
+    <div >
+     dfdfdfd
+
+      
+    </div>
+  )
+}
