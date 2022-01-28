1bcd0f1aae05f0265fbc2e4ca70a5ffc1de6a172
notnotzero <marakhovsky.ivan@gmail.com>
2022-01-28 07:39:34 -0600

    appendix

--- pages/DuckSeason.js
+++ pages/DuckSeason.js
@@ -1,18 +1,14 @@
 import Image from "next/image";
 import styles from "../styles/DuckSeason.module.css";
-import DUCK_SEASON_START from '../public/duckSeason/DUCK SEASON START.png'
+import DUCK_SEASON_START from "../public/duckSeason/DUCK SEASON START.png";
 
-
-export default function First() {
-    return (
-      <div>
-        <div className={styles.duckSeason}>
-          <Image src={DUCK_SEASON_START} />
-        </div>
-        <div className ={styles.emoji}>
-            🌤 🌾 🐣
-        </div>
-        
+export default function DuckSeason() {
+  return (
+    <div>
+      <div className={styles.duckSeason}>
+        <Image src={DUCK_SEASON_START} />
       </div>
-    );
+      <div className={styles.emoji}>🌤 🌾 🐣</div>
+    </div>
+  );
 }
